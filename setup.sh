9c293acbcdc50d542c86a80f96f95b937b077332
Yousaf Nabi <yousafn@gmail.com>
2025-01-26 20:01:18 +0000

    ci: build bookworm arm64 lite

--- setup.sh
+++ setup.sh
@@ -31,11 +31,11 @@ if [ ! -x "$(command -v curl)" ]; then
 fi
 
 
-RASPIOS_OS_VERSION=$(curl -s https://downloads.raspberrypi.org/raspios_lite_arm64/os.json | sed -n 's/.*"version": "\(.*\)"$/\1/p')
-RASPIOS_OS_RELEASE_DATE=$(curl -s https://downloads.raspberrypi.org/raspios_lite_arm64/os.json | sed -n 's/.*"release_date": "\(.*\)",$/\1/p')
+# RASPIOS_OS_VERSION=$(curl -s https://downloads.raspberrypi.org/raspios_lite_arm64/os.json | sed -n 's/.*"version": "\(.*\)"$/\1/p')
+# RASPIOS_OS_RELEASE_DATE=$(curl -s https://downloads.raspberrypi.org/raspios_lite_arm64/os.json | sed -n 's/.*"release_date": "\(.*\)",$/\1/p')
 
-# RASPIOS_OS_VERSION="bookworm"
-# RASPIOS_OS_RELEASE_DATE="2024-11-19"
+RASPIOS_OS_VERSION="bookworm"
+RASPIOS_OS_RELEASE_DATE="2024-11-19"
 
 if [[ -z "$RASPIOS_OS_VERSION" || -z "$RASPIOS_OS_RELEASE_DATE" ]]; then
   echo "Could not determine latest Raspios OS version."
